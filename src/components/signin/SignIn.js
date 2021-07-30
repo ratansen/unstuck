23ad897c631c8d75904cf7d3746086d12cf2eec6
Ratan-Sen <Ratan-Sen@users.noreply.github.com>
2021-07-30 21:54:08 +0530

    router to signin button

--- src/components/signin/SignIn.js
+++ src/components/signin/SignIn.js
@@ -1,11 +1,14 @@
 import React from 'react' ;
+import {Link} from 'react-router-dom'
 import { signInWithGoogle } from '../../firebase/firebase.utils';
 import './signin.css' ;
 
 function SignIn(){
     return(
         <div className='container'>
+        <Link style={{ textDecoration: "none" }} to='/'>
         <div className='signin' onClick={signInWithGoogle}>Sign In with Google</div>
+        </Link>
         </div>
     )
 }
